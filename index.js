2f1eea1a622d7eceffdb6d0f559ebe91f43745dc
markn <markn@aesclever.com>
2022-05-24 15:22:24 -0700

    Interface multer for app.post upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -11,7 +12,16 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+var upload = multer({ dest: "uploads/" });
+app.post("/api/fileanalyse", upload.single("upfile"), function(req, res, next) {
+  var upfile = req.file;
+  if (typeof upfile === "undefined") res.json({ error: "file not uploaded" });
+  return res.json({
+    name: upfile.originalname,
+    type: upfile.mimetype,
+    size: upfile.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
